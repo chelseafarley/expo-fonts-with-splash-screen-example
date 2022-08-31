3e439867de2165cde1d2096565be7e0f60b5c2d0
Chelsea <chelsea@tripwiretech.com>
2022-08-31 15:02:58 +1200

    Expo fonts

--- App.js
+++ App.js
@@ -1,10 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+// expo install expo-font expo-splash-screen
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "LemonLove": require("./assets/fonts/LemonLove.ttf"),
+    "MilkyCoffee": require("./assets/fonts/MilkyCoffee.otf")
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      async function hide() {
+        await SplashScreen.hideAsync();
+      }
+      hide();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return undefined;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: "LemonLove", fontSize: 30 }}>Lemon Love Font</Text>
+      <Text style={{ fontFamily: "MilkyCoffee", fontSize: 30 }}>Milky Coffee Font</Text>
+      <Text>Default Font</Text>
       <StatusBar style="auto" />
     </View>
   );
